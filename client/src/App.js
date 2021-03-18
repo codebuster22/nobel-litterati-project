be86288f0d3ad991349f6be1ca45e4aa0742f1f4
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-03-18 21:30:35 +0530

    front-end-template

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,50 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { Component, useState } from "react";
+import "./App.css";
+import OpenNFTContract from "./contracts/OpenNFT.json";
+import NobelMainContract from "./contracts/NobelMain.json";
+import NobelTokenContract from "./contracts/NobelToken.json";
+
 import getWeb3 from "./getWeb3";
 
-import "./App.css";
+
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { isLoaded: false };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.accounts = await this.web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+      this.networkId = await this.web3.eth.net.getId();
+
+      const OpenNFTNetwork = OpenNFTContract.networks[this.networkId];
+      this.OpenNFTInstance = new this.web3.eth.Contract(
+        OpenNFTContract.abi,
+        OpenNFTNetwork && OpenNFTNetwork.address,
       );
+
+      const NodeMainNetwork = NobelMainContract.networks[this.networkId];
+      this.NodeMainInstance = new this.web3.eth.Contract(
+        NobelMainContract.abi,
+        NodeMainNetwork && NodeMainNetwork.address,
+      );
+
+      // const NodeTokenNetwork = NobelTokenContract.networks[this.networkId];
+      // this.NodeTokenInstance = new this.web3.eth.Contract(
+      //   NobelTokenContract.abi,
+      //   NodeTokenNetwork && NodeTokenNetwork.address,
+      // );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      if(this.web3){
+        this.setState({ isLoaded: true});
+      }
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,39 +54,125 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
   render() {
-    if (!this.state.web3) {
+    if (!this.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div className="App container">
+        <div className={'row'}>
+          <UserStats />
+        </div>
+        <div className={'row'}>
+          <PostLitter />
+          <ViewLitters />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+const UserStats = ({userAddress, totalLitters, nobelBalance}) => {
+
+
+  return (
+        <div className={'user-stats col-12 d-flex flex-wrap justify-content-around'}>
+            <h4 >
+              User Address:- 
+            </h4>
+            <h4>
+              Total Litters Sumbitted:- 
+            </h4>
+            <h4>
+              Nobel Balance:- 
+            </h4>
+        </div>
+  )
+
+
+}
+
+const PostLitter = () => {
+
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [file, setFile] = useState();
+
+
+
+
+  return (
+          <div className={'col-12 col-md-6 post-litter'} >
+            <form >
+              <div className={'custom-file mt-5 mb-3'}>
+                <input 
+                    type={'file'} 
+                    placeholder={"Upload the litter"} 
+                    className={'upload-litter custom-file-input'} 
+                    id={'customFile'} 
+                  />
+                <label 
+                    className={'custom-file-label'}
+                    htmlFor={'customFile'}
+                    >
+                      Pick Up Litter...
+                  </label>
+              </div>
+              {
+                imageLoaded?
+                    <div className={'litter-preview-container mt-3 mb-3 p-2'}>
+                      <img src={''} alt={'litter-preview'} className={'LitterPreview'} />
+                    </div>
+                    :
+                    <></>
+              }
+              <div>
+                <button className={'btn btn-danger mt-3 mb-3'} >
+                  Destroy Litter!
+                </button>
+              </div>
+            </form>
+          </div>
+  )
+
+
+}
+
+
+const ViewLitters = ({litters}) => {
+
+
+
+  return (
+          <div className={'col-12 col-md-6 view-litters'} >
+            <div className={'mt-5 mb-5 p-2 d-flex justify-content-center'}>
+                <LitterCard />
+            </div>
+          </div>
+  )
+
+}
+
+const LitterCard = ({image, address}) => {
+
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  return (
+          <div className="card" style={{width: '18rem'}}>
+                  {
+                    imageLoaded?
+                        <img src="..." className="card-img-top" alt="..." />
+                        :
+                        <></>
+                  }
+                  <div className="card-body">
+                    <h5 className="card-title">User Address</h5>
+                    <a href="#" className="btn btn-primary">Give Reward</a>
+                  </div>
+            </div>
+  )
+
+
+}
