73029c305f134de4a1f44561166519556433794f
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-04-08 19:25:29 +0530

    Updated client/footer to support both links

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
-const CLIENT = process.env.REACT_APP_REPOSITORY_URL;
+const CLIENT = process.env.REACT_APP_CLIENT_REPOSITORY_URL;
 const PROJECT = process.env.REACT_APP_PROJECT_REPOSITORY_URL;
 
 
@@ -15,6 +15,7 @@ const Footer = () =>
                 Created with <FontAwesomeIcon icon={faHeart} className={'heart'} /> by Codebuster22.
                 <br/>
                 <a href={CLIENT} target="_blank" rel="noopener noreferrer">Link to Client Side Repo!</a>
+                <br/>
                 <a href={PROJECT} target="_blank" rel="noopener noreferrer">Link to Full Project Repo!</a>
         </p>
 
